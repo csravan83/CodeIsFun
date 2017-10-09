e3be86eb26d12dea0760e9066efcddeeebf5f605
Wanlipa Thongsuntia <nuam18@gmail.com>
2017-10-09 22:06:49 +0700

    ps3 controller

--- ps3/config/routes.rb
+++ ps3/config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-
+  get 'site/index'
+  root 'site#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
-
-  get 'users/admin'
-  get 'users/user'
-  get 'users/user1'
 end
