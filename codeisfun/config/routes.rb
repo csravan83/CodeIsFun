8a4798d3815495e72f748d49570f4d4b3a1ac992
Wanlipa Thongsuntia <nuam18@gmail.com>
2017-10-23 12:08:13 +0700

    add document for ps4

--- codeisfun/config/routes.rb
+++ codeisfun/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
+  # mount "sites/index" => redirect("https://web1.cs.ait.ac.th/"), :constraints => { :protocol => "http://" }
+  # mount "sites/index" => redirect("https://web1.cs.ait.ac.th/"), :constraints => { :subdomain => "" }
+
   get 'users/index'
   get 'problem/page'
   get 'sites/index'
