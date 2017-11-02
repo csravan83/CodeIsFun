1ed4a9ff08e14a4e8360fae4890f9344f3d153a6
Wanlipa Thongsuntia <nuam18@gmail.com>
2017-11-02 15:03:43 +0700

    update gitignore and add gem papertrail for versioning

--- codeisfun/app/controllers/categories_controller.rb
+++ codeisfun/app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :set_paper_trail_whodunnit
+
   def page
   end
 end
--- codeisfun/app/models/ability.rb
+++ codeisfun/app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
     can :read, :all                   # allow everyone to read everything
 
     if user.admin?
-      can :access, :rails_admin       # only allow admin users to access Rails Admin
+      can :access, :rails_admin     # only allow admin users to access Rails Admin
       can :dashboard                  # allow access to dashboard
       can :manage, :all
     else
