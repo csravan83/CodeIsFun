3ba06dd76c75d768a48ac20c7aa1310a031b3567
Wanlipa Thongsuntia <nuam18@gmail.com>
2017-11-03 21:15:16 +0700

    UATs Scenario: Admin can create category and problem

--- codeisfun/features/admin.feature
+++ codeisfun/features/admin.feature
@@ -3,32 +3,37 @@ Feature: admin
 
   Background:
     Given I am an Admin
-
-  Scenario: Admin can login to codeisfun page
-
     When I visit the root page
     Then I should see the login Link
     When I click the login link
     Then I Should see a sign in form
     When I fill in the details and submit
+
+  Scenario: Admin can access the Admin Dashboard
+
     Then I Should see the Admin Dashboard Link
     When I Click on Admin Dashboard
     Then I should see the User model
     When I click on user model
 
+  Scenario: Admin can add category and create the problem
+    And  There is a category
+    And  I want to add the problem
+    Then I should see the problem
+    When I click on the problems link
+    Then I should see the create new category button
+    When I click on the create new category button
+    Then I should see the input form
+    When I fill in the category name and submit
+    Then I should see new category button
+    When I click on the category
+    Then I should see an add problem button
+    When I click the add problem button
+    Then I should see the problem form
+    When I fill in the problem details and submit
+    Then I should see the details of problem
+
 #  Scenario: Admin can ban users
 
-#  Scenario: Admin can create the problem
-#    Given I am an Admin
-#    When  I logged in and see my name at root page
-#    Then  I should see the problems Link
-#    When  I click on the problems Link
-#    Then  I should see the list of categories
-#    When  I click one of the category
-#    Then  I should see an add button
-#    When  I click the add button
-#    Then  I should see a problem form
-#    When  I fill in the details and submit form
-#    Then  I should see the problem list
-#
+
 
--- codeisfun/features/step_definitions/admin_steps.rb
+++ codeisfun/features/step_definitions/admin_steps.rb
@@ -21,6 +21,8 @@ When("I fill in the details and submit") do
   #save_and_open_page
 end
 
+# Scenario: Admin can access the Admin Dashboard
+
 Then("I Should see the Admin Dashboard Link") do
   expect(page).to have_link("Admin Dashboard")
 end
@@ -39,6 +41,87 @@ When("I click on user model") do
   # save_and_open_page
 end
 
+# Scenario: Admin can add category and create the problem
+
+When("There is a category") do
+  @category = FactoryGirl.create :category
+end
+
+When("I want to add the problem") do
+  @problem = FactoryGirl.create :problem
+end
+
+Then("I should see the problem") do
+  expect(page).to have_link('Problems')
+end
+
+When("I click on the problems link") do
+    click_on("Problems")
+end
+
+Then("I should see the create new category button") do
+  expect(page).to have_link('Create New Category')
+end
+
+When("I click on the create new category button") do
+  find_link('Create New Category').click
+end
+
+Then("I should see the input form") do
+  expect(page).to have_selector("form")
+end
+
+When("I fill in the category name and submit") do
+  fill_in('Name', :with => 'Data Structure')
+  click_button 'Create'
+end
+
+Then("I should see new category button") do
+  expect(page).to have_link(@category.name)
+end
+
+When("I click on the category") do
+  click_on(@category.name)
+end
+
+Then("I should see an add problem button") do
+  expect(page).to have_link('Add Problem')
+end
+
+When("I click the add problem button") do
+  click_on('Add Problem')
+end
+
+Then("I should see the problem form") do
+  expect(page).to have_selector("form")
+end
+
+When("I fill in the problem details and submit") do
+  find('#problem_category_id').find(:option, @problem.category.name).select_option
+  fill_in 'Title', with: @problem.title
+  fill_in 'Description', with: @problem.description
+  fill_in 'Input', with: @problem.input
+  fill_in 'Output', with: @problem.output
+  fill_in 'Tc1', with: @problem.TC1
+  fill_in 'Tc2', with: @problem.TC2
+  fill_in 'Tc3', with: @problem.TC3
+  page.find(:select, name: 'problem[status]').find(:option, @problem.status).select_option
+  fill_in 'Author', with: @problem.author
+  click_button 'Create Problem'
+  # save_and_open_page
+end
+
+Then("I should see the details of problem") do
+  expect(page).to have_content "Title: #{@problem.title}"
+  expect(page).to have_content "Description: #{@problem.description}"
+  expect(page).to have_content "Input: #{@problem.input}"
+  expect(page).to have_content "Output: #{@problem.output}"
+  expect(page).to have_content "Tc1: #{@problem.TC1}"
+  expect(page).to have_content "Tc2: #{@problem.TC2}"
+  expect(page).to have_content "Tc3: #{@problem.TC3}"
+  expect(page).to have_content "Status: #{@problem.status}"
+  expect(page).to have_content "Author: #{@problem.author}"
+end
 
 
 
